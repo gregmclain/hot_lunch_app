0181943bbf3b6c19404adc025b99db4a38b55025
Greg McLain <mclain.greg@gmail.com>
2015-06-02 18:51:15 -0500

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   resources :students
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-    root 'students#index'
+    root 'static_pages#home'
 
 # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
