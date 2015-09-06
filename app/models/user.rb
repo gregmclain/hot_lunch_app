ca7b72cf119c3bc23c19fbdbdb2f09e12e3e451e
Greg McLain <mclain.greg@gmail.com>
2015-09-05 19:33:36 -0500

    Add user authentication using Devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ActiveRecord::Base
-  validates :name,  presence: true
-  before_save { self.email = email.downcase }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false}
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :students, dependent:   :destroy
 end
