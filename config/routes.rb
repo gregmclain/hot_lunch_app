0186d7a2539d915461f1ac2be1f2503cdb7a3013
Greg McLain <mclain.greg@gmail.com>
2015-11-13 18:58:11 -0600

    Remove unnecessary routes and remove SSL requirement
    SSL cannot be used because we don't have a certificate.

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,6 @@ Rails.application.routes.draw do
     resources :orders
   end
 
-  resources :menus
-  resources :items
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
