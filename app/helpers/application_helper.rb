3ccb5b778c3fa1c32e7f4ac474cb3175796c1e57
Greg McLain <mclain.greg@gmail.com>
2015-11-13 18:53:28 -0600

    Add footer and change color of header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,26 @@ module ApplicationHelper
     end
   end
 
+
+  BOOTSTRAP_FLASH_MSG = {
+      success: 'alert-success',
+      error: 'alert-error',
+      alert: 'alert-block',
+      notice: 'alert-info'
+  }
+
+  def bootstrap_class_for(flash_type)
+    BOOTSTRAP_FLASH_MSG.fetch(flash_type.to_sym, flash_type.to_s)
+  end
+
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+               concat message
+             end)
+    end
+    nil
+  end
+
 end
