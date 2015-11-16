aed1613b0de3d12c4ec23d1b0133caff1e1907bf
Greg McLain <mclain.greg@gmail.com>
2015-11-15 22:56:22 -0600

    Fix whitelisted parameters
    The orders controller was allowing any parameters through. It is now
    restricted to only desired params.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -72,8 +72,8 @@ class OrdersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def order_params
-    #params.require(:order).permit(:order_date, :student_id)
-    params.require(:order).permit!
+    params.require(:order).permit(:order_date, :student_id, :entree_quantity, :entree,
+                                  :side, :dessert, :price)
   end
 
   def correct_user
