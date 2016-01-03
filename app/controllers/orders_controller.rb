fbd76a3c3805b0b2848ca52d4cdd458f8ac47497
Greg McLain <mclain.greg@gmail.com>
2016-01-02 21:46:55 -0600

    Change deadline

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,7 +18,7 @@ class OrdersController < ApplicationController
     @order.order_date = params[:menu_date]
     @menu = Menu.find_by(menu_date: params[:menu_date])
     set_menu_items
-    if @order.order_date < Date.current || (Date.current.day > 3 && Date.current.month == 1)
+    if @order.order_date < Date.current || (Date.current.day > 4 && Date.current.month == 1)
         redirect_to @student, notice: 'Ordering is complete for this month.'
     end
   end
@@ -89,7 +89,7 @@ class OrdersController < ApplicationController
   end
 
   def check_date_range
-    if @order.order_date < Date.current || (Date.current.day > 3 && Date.current.month == 1)
+    if @order.order_date < Date.current || (Date.current.day > 4 && Date.current.month == 1)
       redirect_to student_order_path(@student, @order)
     end
   end
